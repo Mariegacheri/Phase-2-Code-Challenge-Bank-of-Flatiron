d60b56069ab257d20540e421d4eb099c0e00f2dc
marie <mariegacheri@gmail.com>
2023-11-15 10:25:00 +0300

    completed assignment

--- src/App.js
+++ src/App.js
@@ -2,41 +2,44 @@ import React, { useState, useEffect } from 'react';
 import TransactionTable from './TransactionTable';
 import TransactionForm from './TransactionForm';
 import SearchBar from "./SearchBar";
-import transactionsData from './data'; 
 import './App.css';
-
-useEffect(() => {
-  // Fetch data from the local json server
-  fetch('https://my-json-server.typicode.com/mariegacheri/Phase-2-Code-Challenge-Bank-of-Flatiron/transactions')
-    .then(response => response.json())
-    .then(data => {
-      setTransactions(data);
-      setFilteredTransactions(data);
-    })
-    .catch(error => console.error('Error fetching data:', error));
-  }, []);  //loops continiously hence the array so as to run once
 
 const App = () => {
   const [transactions, setTransactions] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [filteredTransactions, setFilteredTransactions] = useState([]);
 
   useEffect(() => {
-    setTransactions(transactionsData); 
+    // Fetch data from the local json server
+    fetch('https://my-json-server.typicode.com/mariegacheri/Phase-2-Code-Challenge-Bank-of-Flatiron/transactions')
+      .then(response => response.json())
+      .then(data => {
+        if (Array.isArray(data)) {
+          setTransactions(data);
+          setFilteredTransactions(data);
+        } else {
+          console.error('Data is not an array:', data);
+        }
+      })
+      .catch(error => console.error('Error fetching data:', error));
   }, []);
-
+  
   const handleAddTransaction = (newTransaction) => {
     setTransactions([...transactions, newTransaction]);
   };
 
-  const filteredTransactions = transactions.filter((transaction) =>
-    transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const handleSearchChange = (newSearchTerm) => {
+    setSearchTerm(newSearchTerm);
+    setFilteredTransactions(transactions.filter((transaction) =>
+      transaction.description.toLowerCase().includes(newSearchTerm.toLowerCase())
+    ));
+  };
 
   return (
     <div id='parentdiv'>
-      <div id='Siteheader'><h1>FLAT-IRON BANK</h1></div>
+      <div id='Siteheader'><h1>FLATIRON BANK</h1></div>
       <TransactionForm onAddTransaction={handleAddTransaction} />
-      <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />
+      <SearchBar searchTerm={searchTerm} onSearchChange={handleSearchChange} />
       <TransactionTable transactions={filteredTransactions} />
     </div>
   );
